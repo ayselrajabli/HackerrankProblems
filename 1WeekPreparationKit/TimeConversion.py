1a17ce637997a3e2b246e74f692c459248d32520
Aysel Rajabli <95722237+ayselrajabli@users.noreply.github.com>
2023-04-05 11:57:32 +0400

    Create TimeConversion.py

---
+++ 1WeekPreparationKit/TimeConversion.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if s[:2]=='12':
+        if s[-2]=='P':
+            return s[:-2]
+        else:
+            return f'00{s[2:-2]}'
+    else:
+        if s[-2]=='P':
+            return f'{int(s[:2]) + 12}{s[2:-2]}'
+        else:
+            return  s[:-2]
+            
+            
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
